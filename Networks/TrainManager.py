dc29dee2ade649bb166bb2357b5e7155cd05c169
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-07-27 19:14:07 +0300

    change model architecture by classes num and dropout

--- Networks/TrainManager.py
+++ Networks/TrainManager.py
@@ -12,6 +12,7 @@ class TrainTestHelper:
         self.loss_func = loss_func
         self.loss_logger = tf.keras.metrics.Mean(name='loss')
         self.training = training
+        self.steps =  []
 
     def update_optimizer(self, optimizer):
         self.optimizer = optimizer
@@ -23,14 +24,31 @@ class TrainTestHelper:
         @tf.function()
         def train_step(inputs, labels):
             with tf.GradientTape(persistent=True) as tape:
+                self.steps.append(0)
 
                 prediction = self.model(inputs, training=self.training)
+                self.steps.append(1)
+
                 loss_value = self.loss_func(labels, prediction)
+                self.steps.append(3)
+
                 self.loss_logger(loss_value)
+                self.steps.append(4)
+
 
             if self.training:
+                self.steps.append(6)
+
                 grads = tape.gradient(loss_value, self.model.trainable_variables)
+                self.steps.append(7)
+
                 self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+                self.steps.append(8)
+
         return train_step
+
+    def __del__(self):
+        with open("text.txt", 'w') as f:
+            f.write(str(self.steps))
 
 
