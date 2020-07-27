8f9209efdec775e76c8b840677a13605100f3f51
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-07-27 21:52:56 +0300

    change model architecture by classes num and dropout

--- Networks/TrainManager.py
+++ Networks/TrainManager.py
@@ -30,7 +30,7 @@ class TrainTestHelper:
 
             if self.training:
                 grads = tape.gradient(loss_value, self.model.trainable_variables)
-                # self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+                self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
 
         return train_step
 
