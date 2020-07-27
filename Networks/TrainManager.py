6f4c71e450214c0ca7c6be24900351c794927895
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-07-27 19:03:34 +0300

    change model architecture by classes num and dropout

--- Networks/TrainManager.py
+++ Networks/TrainManager.py
@@ -26,12 +26,12 @@ class TrainTestHelper:
 
                 # Descriptiveness loss
                 prediction = self.model(inputs, training=self.training)
-                loss_value = self.loss_func(labels, prediction)
-                self.loss_logger(loss_value)
-
-            if self.training:
-                grads = tape.gradient(loss_value, self.model.trainable_variables)
-                self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+            #     loss_value = self.loss_func(labels, prediction)
+            #     self.loss_logger(loss_value)
+            #
+            # if self.training:
+            #     grads = tape.gradient(loss_value, self.model.trainable_variables)
+            #     self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
         return train_step
 
 
